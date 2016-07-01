8bbc1335f72af765671bab6f6cbac41cafacd2b7
klebson09 <silvaklebson_@hotmail.com>
2016-07-01 11:24:35 -0300

    Implementação de CadastroMedico

--- src/java/servlet/ControladorDAO.java
+++ src/java/servlet/ControladorDAO.java
@@ -4,8 +4,10 @@ import dao.*;
 import java.util.ArrayList;
 import java.util.List;
 import model.Endereco;
+import model.Medico;
 import model.Paciente;
 import model.Pessoa;
+import servlet.facade.FacadeMedico;
 import servlet.facade.FacadePaciente;
 
 /**
@@ -20,7 +22,7 @@ public class ControladorDAO {
 
     }
 
-    protected void construtorDeObjetos(Pessoa pessoa, Paciente paciente, String classe) {
+    protected void construtorDeObjetos(Pessoa pessoa, Paciente paciente, Medico medico, String classe) {
 //        Endereco endereco;ArrayList<Object> objetos
 
 //        for (int i = 0; i < objetos.size(); i++) {
@@ -29,14 +31,12 @@ public class ControladorDAO {
 //                pessoa = (Pessoa) objetos.get(i);
 //            } else if (objetos.get(i).getClass() == Paciente.class) {
 //                paciente = (Paciente) objetos.get(i);
-
 //            }else if(objetos.get(i).getClass()== Medico.class){
 //                Medico medico = new Medico();
 //                medico = (Medico) objetos.get(i);
 //                
 //            } else if (objetos.get(i).getClass() == Endereco.class) {
 //                endereco = (Endereco) objetos.get(i);
-
 //            }
 //
 //        }
@@ -44,8 +44,15 @@ public class ControladorDAO {
             case "Paciente":
                 FacadePaciente facadePaciente = new FacadePaciente();
                 facadePaciente.AdicionarPaciente(pessoa, paciente);
+            case "Medico":
+                FacadeMedico facadeMedico = new FacadeMedico();
+                facadeMedico.AdicionarMedico(pessoa, medico);
         }
 
+    }
+
+    void construtorDeObjetos(Pessoa pessoa, Medico medico, String medico0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
--- src/java/servlet/ControllerCadastroMedico.java
+++ src/java/servlet/ControllerCadastroMedico.java
@@ -9,40 +9,40 @@ import dao.DAOPessoa;
 import model.Pessoa;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
 //import java.text.SimpleDateFormat;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Medico;
 
-public class ControllerPessoa extends HttpServlet {
+public class ControllerCadastroMedico extends HttpServlet {
 
-   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        String nome = request.getParameter("nome");
         //todo valor que vem do formulario html ou jsp é string e precisa converter para int
         //obs. tipo data, tbm converter
-        
-// FALTA VERIFICAR A CONVERSÃO DA VARIAVEL dataNasc para o tipo Date        
-        String dataNasc = request.getParameter("dataNasc");//pegando dados de um formulário WEB 
-        //SimpleDateFormat sdf1= new SimpleDateFormat("dd/MM/yyyy"); //você pode usar outras máscaras 
-        //Date dataNasc = sdf1.parse(dataNasc);
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
 
-        String cpf = request.getParameter("cpf");
+        String nome = request.getParameter("nome");
+        String dataNasc = request.getParameter("dataNasc");//pegando dados de um formulário WEB 
         String rg = request.getParameter("rg");
+        String cpf = request.getParameter("cpf");
+//        String telefone = request.getParameter("telefone");
+//        String celular = request.getParameter("celular");
         String email = request.getParameter("email");
+        String numCRM = request.getParameter("numCRM");
         String pwd = request.getParameter("pwd");
-        System .out.println("mostra nome: " +nome+ "  dat nascimento: :" + dataNasc + "  CPF: " + cpf + "  Rg: " + rg + "  EMAIL: " + email + "  SENHA: " + pwd + "  ");
 //        System .out.println("mostra logradouro " +nome+ "complemento:" + dataNasc + "n:" + cpf + "cidade:" + rg + "estado:" + email + "cep:" + pwd + "");
+
+        Medico medico = new Medico();
+        medico.setNumCRM(numCRM);
+
         Pessoa pessoa = new Pessoa();
-        // pessoa.setIdPessoa(Integer.MIN_VALUE);
-        //converter string para data
-//        pessoa.setDataNasc(dataNasc);
         pessoa.setNome(nome);
         pessoa.setDataNasc(dataNasc);
         pessoa.setCpf(cpf);
@@ -50,8 +50,15 @@ public class ControllerPessoa extends HttpServlet {
         pessoa.setEmail(email);
         pessoa.setPwd(pwd);
 
-        DAOPessoa daopessoa = new DAOPessoa();
-        daopessoa.adicionar(pessoa);
+        ArrayList<Object> listaObjetos = new ArrayList<>();
+        listaObjetos.add(pessoa);
+        listaObjetos.add(medico);
+
+        ControladorDAO controladorDao = new ControladorDAO();
+
+        controladorDao.construtorDeObjetos(pessoa, medico, "Medico");
+        RequestDispatcher rd = request.getRequestDispatcher("jsps/cadastroSucesso.jsp");
+        rd.forward(request, response);
 
     }
 }
